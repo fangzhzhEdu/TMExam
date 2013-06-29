8a8c634e01df08e6cccf3daed9cc244f38d2d78c
ZHANG SHI GUANG <andyzhshg@gmail.com>
2013-06-29 23:25:27 +0800

    Initial Commit

---
+++ TMExam/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TMExam
+//
+//  Created by ZHANG SHI GUANG on 13-6-29.
+//  Copyright (c) 2013年 ZHANG SHI GUANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
